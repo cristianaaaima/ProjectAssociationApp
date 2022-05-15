29f403936d252db8ee9eff33582999d9ba317bb5
AlinaNovac <72496629+AlinaNovac@users.noreply.github.com>
2022-05-15 15:29:45 +0300

    Adding first part of identity

--- SEAssociationApp/SEAssociationApp/Program.cs
+++ SEAssociationApp/SEAssociationApp/Program.cs
@@ -5,16 +5,50 @@ using SEProjectApp.Abstractions.Repository;
 using SEProjectApp.DataAccess;
 
 var builder = WebApplication.CreateBuilder(args);
+//var connectionString = builder.Configuration.GetConnectionString("AssociationContextConnection") ?? throw new InvalidOperationException("Connection string 'AssociationContextConnection' not found.");
 
-//Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+//builder.Services.AddDbContext<AssociationContext>(options =>
+//    options.UseSqlServer(connectionString));;
 
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<AssociationContext>();builder.Services.AddDbContext<AssociationContext>
+    (options => options.UseSqlServer(builder.Configuration.GetConnectionString("ProjectDb")));
+
+//Add services to the container.!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//    options.UseSqlServer(connectionString));
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+// Password settings
+options.Password.RequireDigit = true;
+options.Password.RequiredLength = 8;
+options.Password.RequireNonAlphanumeric = false;
+options.Password.RequireUppercase = true;
+options.Password.RequireLowercase = false;
+options.Password.RequiredUniqueChars = 6;
+
+// Lockout settings
+options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+options.Lockout.MaxFailedAccessAttempts = 10;
+    options.Lockout.AllowedForNewUsers = true;
+
+    // User Settings
+    options.User.RequireUniqueEmail = true;
+});
+
+
+
+
+//builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+//builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+//    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 //var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 //var connectionStringIdentity = builder.Configuration.GetConnectionString("IdentityConnection");
@@ -31,7 +65,7 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
 //builder.Services.AddScoped<IBuildingRepository, BuildingRepository>();
 //builder.Services.AddScoped<IInvoiceRepository, InvoiceRepository>(); 
 
-builder.Services.AddControllersWithViews();
+
 
 
 
@@ -57,9 +91,18 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllerRoute(
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}");
+//app.MapRazorPages();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
+    endpoints.MapRazorPages();
+});
+
 
 app.Run();
