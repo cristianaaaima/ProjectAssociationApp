e8c3cce5c9506a8b87ea016f557ebe91fb1ff365
AlinaNovac <72496629+AlinaNovac@users.noreply.github.com>
2022-05-15 19:34:04 +0300

    Adding controllers and views for models

--- SEAssociationApp/SEProjectApp.DataModel/Building.cs
+++ SEAssociationApp/SEProjectApp.DataModel/Building.cs
@@ -10,12 +10,12 @@ namespace SEProjectApp.DataModel
     {
         public int BuildingId { get; set; }
 
-        public string StreetName { get; set; }
+        public string? StreetName { get; set; }
 
-        public string StreetNo { get; set; }
+        public string? StreetNo { get; set; }
 
-        public string BuildingNo { get; set; }
+        public string? BuildingNo { get; set; }
 
-        public ICollection<Apartment> Apartments { get; set; }
+        public ICollection<Apartment>? Apartments { get; set; }
     }
 }
